01a4f86498a61d12f9e84a96dcaac2c9a1d31543
Yanina <crzyanina@gmail.com>
2023-07-04 21:47:38 -0400

    lesson-1-4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 
-const App = () => (
-      <div>
-      <h1>Todo List</h1>
-     <AddTodoForm /> 
-     <TodoList />
+const App = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
+
+return (
+  <div>
+    <h1>Todo List</h1>
+    <AddTodoForm onAddTodo={addTodo} />
+    <TodoList todoList={todoList} />
      </div>
   );
+};
 
 export default App;
